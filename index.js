2ac8aaabee411420327933000384af73c91ca995
emilyjia <emilyjia2010@gmail.com>
2020-01-11 21:30:04 -0800

    added camera

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {RNCamera} from 'react-native-camera';
+
+var Camera = require("react-native-camera");
 
 AppRegistry.registerComponent(appName, () => App);
